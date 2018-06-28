271c3187bb4f157cd8fb42ecf7b00ac8b842dda5
orangezeit <40207006+orangezeit@users.noreply.github.com>
2018-06-28 11:31:58 +0800

    Create 0326-Power-of-Three.cpp

---
+++ C/0326-Power-of-Three.cpp
@@ -0,0 +1,15 @@
+    bool isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        
+        while (n != 1) {
+            if (n % 3 != 0) {
+                return false;
+            } else {
+                n = n / 3;
+            }
+        }
+        
+        return true;
+    }
