55ab11e298aec957ee8cbd78b7376373d1e44eff
orangezeit <40207006+orangezeit@users.noreply.github.com>
2018-06-28 10:24:40 +0800

    Update 0242-Valid-Anagram.cpp

--- C/0242-Valid-Anagram.cpp
+++ C/0242-Valid-Anagram.cpp
@@ -2,13 +2,13 @@
         if (s.length() != t.length()) {
             return false;
         } else {
-            vector<int> sletters(26, 0), tletters(26, 0);
+            vector<int> s_letters(26, 0), t_letters(26, 0);
             
             for (int i = 0; i < s.length(); ++i) {
-                sletters[s.at(i)-'a']++;
-                tletters[t.at(i)-'a']++;
+                s_letters[s[i]-'a']++;
+                t_letters[t[i]-'a']++;
             }
             
-            return sletters == tletters;
+            return s_letters == t_letters;
         }
     }
