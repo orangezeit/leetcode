c5cb0d9b119ccefa8872759eb1a0b4bdc7a9c922
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-06-28 19:05:33 +0800

    Create 0006-Zigzag-Conversion.cpp

---
+++ C/0006-Zigzag-Conversion.cpp
@@ -0,0 +1,33 @@
+    string convert(string s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        
+        string s2;
+        string strs[numRows];
+        
+        int k(0);
+        bool inverse(false);
+        
+        for (int i = 0; i < s.length(); ++i) {
+            if (inverse) {
+                strs[k--] += s[i];
+                if (k == -1) {
+                    k += 2;
+                    inverse = false;
+                }
+            } else {
+                strs[k++] += s[i];
+                if (k == numRows) {
+                    k -= 2;
+                    inverse = true;
+                }
+            }
+        }
+        
+        for (int i = 0; i < numRows; ++i) {
+            s2 += strs[i];
+        }
+        
+        return s2;
+    }
