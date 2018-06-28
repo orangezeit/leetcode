808cd8632606e0a265a6a4cb9fb2727aaec61994
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-06-28 19:09:53 +0800

    Create 0003-Longest-Substring-Without-Repeating-Characters.cpp

---
+++ C/0003-Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,22 @@
+    int lengthOfLongestSubstring(string s) {
+        
+        unordered_set<char> list;
+        int maxL(0), l(0);
+        
+        for (int i = 0; i < s.length(); ++i) {
+            if (list.find(s[i]) == list.end()) {
+                list.insert(s[i]);
+                l++;
+            } else {
+                maxL = max(maxL, l);
+                
+                while (s[i-l] != s[i]) {
+                    list.erase(s[i-l]);
+                    l--;
+                }
+                
+            }
+        }
+        
+        return max(maxL, l);
+    }
