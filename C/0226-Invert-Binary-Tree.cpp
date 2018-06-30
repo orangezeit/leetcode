3dffd2805a8ab44f352167cb4fc4a7fc0333740c
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-06-30 23:30:31 +0800

    Update 0226-Invert-Binary-Tree.cpp

--- C/0226-Invert-Binary-Tree.cpp
+++ C/0226-Invert-Binary-Tree.cpp
@@ -1,16 +1,10 @@
     TreeNode* invertTree(TreeNode* root) {
         if (!root) {
             return root;
-        } else {
-            TreeNode* temp = root->left;
-            root->left = root->right;
-            root->right = temp;
-            if (root->left) {
-                root->left = invertTree(root->left);
-            }
-            if (root->right) {
-                root->right = invertTree(root->right);
-            }
-            return root;
         }
+        
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
     }
