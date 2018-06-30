885aa12b1e45744583d8f64401e233e082ed16ef
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-06-30 16:30:18 +0800

    Update 0001-Two-Sum.cpp

--- C/0001-Two-Sum.cpp
+++ C/0001-Two-Sum.cpp
@@ -1,43 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i, j, k, c(nums.size());
-        vector<int> nums_copy(nums.size()), record(2);
-
-        nums_copy = nums;
-        sort(nums.begin(), nums.end());
+        unordered_map<int, int> record;
         
-        for (i = 0; i < nums.size(); ++i) {
-            if (target <= nums[i] - nums[0]) {
-                c = i;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (record.find(target - nums[i]) == record.end()) {
+                record[nums[i]] = i;
+            } else {
+                return vector<int>({record[target - nums[i]], i});
             }
         }
-        
-        for (i = 0; i < c; ++i) {
-            for (j = i + 1; j < c; ++j) {
-                if (nums[i] + nums[j] == target) {
-                    if (nums[i] != nums[j]) {
-                       for (k = 0; k < nums.size(); ++k) {
-                            if (nums[i] == nums_copy[k]) {
-                                record[0] = k;
-                            } else if (nums[j] == nums_copy[k]) {
-                                record[1] = k;
-                            }
-                        }
-                    } else {
-                        for (k = 0; k < nums.size(); ++k) {
-                            if (nums[i] == nums_copy[k]) {
-                                record[0] = k;
-                                nums[i]++;
-                            } else if (nums[j] == nums_copy[k]) {
-                                record[1] = k;
-                            }
-                        }
-                    }
-                    return record;
-                }
-            }
-        }
-        
     }
 };
