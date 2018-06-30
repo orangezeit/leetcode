772f2b55f2806eab16904333ff9edc1105d9aa38
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-06-30 13:40:36 +0800

    Create 0268-Missing-Number.cpp

---
+++ C/0268-Missing-Number.cpp
@@ -0,0 +1,8 @@
+    int missingNumber(vector<int>& nums) {
+        int n(nums.size()), sum(0);
+        
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+        }
+        return n*(n+1)/2 - sum;
+    }
