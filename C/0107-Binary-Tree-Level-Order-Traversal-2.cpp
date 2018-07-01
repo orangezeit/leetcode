c89fad1a39a203a6008d9b064fc7d0a5268e62c2
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-01 18:16:00 +0800

    Create 0107-Binary-Tree-Level-Order-Traversal-2.cpp

---
+++ C/0107-Binary-Tree-Level-Order-Traversal-2.cpp
@@ -0,0 +1,29 @@
+private:
+    vector<vector<int>> orders;
+    
+    void record(TreeNode* root, int level) {
+        if (root == NULL) {
+            return;
+        }
+        
+        if (orders.size() == level) {
+            orders.push_back({root->val});
+        } else {
+            orders[level].push_back(root->val);
+        }
+        
+        if (root->left) {
+            record(root->left, level+1);
+        } 
+        
+        if (root->right) {
+            record(root->right, level+1);
+        }
+    }
+    
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        record(root, 0);
+        reverse(orders.begin(), orders.end());
+        return orders;
+    }
