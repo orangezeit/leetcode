a4856d231bace7b1651e828433bdd9415e0b01aa
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-02 10:38:16 +0800

    Update 0046-Permutations.cpp

--- C/0046-Permutations.cpp
+++ C/0046-Permutations.cpp
@@ -1,25 +1,22 @@
-    void permuteHelper(vector<int> nums, vector<int> permute, vector<vector<int>>& permutes) {
+private:
+    vector<vector<int>> permutes;
+public:
+    void permuteHelper(vector<int> nums, vector<int> permute) {
         if (nums.empty()) {
             permutes.push_back(permute);
         } else {
             for (int i = 0; i < nums.size(); ++i) {
-                int k = nums[i];
-                permute.push_back(k);
+                permute.push_back(nums[i]);
                 nums.erase(nums.begin()+i);
-                
-                permuteHelper(nums, permute, permutes);
-                
+                permuteHelper(nums, permute);
+                nums.insert(nums.begin()+i, permute.back());
                 permute.pop_back();
-                nums.insert(nums.begin()+i, k);
+                
             }
         }
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> permute;
-        vector<vector<int>> permutes;
-        
-        permuteHelper(nums, permute, permutes);
-        
+        permuteHelper(nums, permute);
         return permutes;
     }
