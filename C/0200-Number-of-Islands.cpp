14e0879e1fd67226bf66f12a24d4d92c438db1bf
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-02 15:53:22 +0800

    Create 0200-Number-of-Islands.cpp

---
+++ C/0200-Number-of-Islands.cpp
@@ -0,0 +1,45 @@
+    void island(vector<vector<char>>& grid, int i, int j) {
+        if (j != 0) {
+            if (grid[i][j-1] == '1') {
+                grid[i][j-1] = '2';
+                island(grid, i, j-1);
+            }
+        }
+        
+        if (j != grid[0].size()-1) {
+            if (grid[i][j+1] == '1') {
+                grid[i][j+1] = '2';
+                island(grid, i, j+1);
+            }
+        }
+        
+        if (i != 0) {
+            if (grid[i-1][j] == '1') {
+                grid[i-1][j] = '2';
+                island(grid, i-1, j);
+            }
+        }
+        
+        if (i != grid.size()-1) {
+            if (grid[i+1][j] == '1') {
+                grid[i+1][j] = '2';
+                island(grid, i+1, j);
+            }
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int num = 0;
+        
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') {
+                    island(grid, i, j);
+                    num++;
+                }
+            }
+        }
+        
+        return num;
+    }
