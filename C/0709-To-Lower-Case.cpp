b4b4d7eb0d9c19268127bf5c8b97954af2478856
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-14 16:35:41 +0800

    Create 0709-To-Lower-Case.cpp

---
+++ C/0709-To-Lower-Case.cpp
@@ -0,0 +1,8 @@
+    string toLowerCase(string str) {
+        for (int i = 0; i < str.length(); ++i) {
+            if (str[i] >= 'A' && str[i] <= 'Z') {
+                str[i] += 32;
+            }
+        }
+        return str;
+    }
