a717ef2e66e2f71798e452fbe99d88e128def486
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-18 17:58:51 +0800

    Update 0069-Sqrt(x).py

--- python/0069-Sqrt(x).py
+++ python/0069-Sqrt(x).py
@@ -1,13 +1,9 @@
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
         i = 0
         j = 46341
         
         while i + 1 < j:
-            m = int((i+j)/2)
+            m = (i+j)//2
             if x < m * m:
                 j = m
             else:
