872340677e237cfa66f9ede1ac3793168e2b43de
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-19 17:50:44 +0800

    Create 0028-Implement-strStr().cpp

---
+++ C/0028-Implement-strStr().cpp
@@ -0,0 +1,9 @@
+    int strStr(string haystack, string needle) {
+        for (int i = 0; i < haystack.length() - needle.length() + 1; ++i) {
+            if (haystack.substr(i, needle.length()) == needle) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
