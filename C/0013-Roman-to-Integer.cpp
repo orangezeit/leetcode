127d42b4bba411e6e3e9607472277c1c23b692ff
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-19 17:25:57 +0800

    Create 0013-Roman-to-Integer.cpp

---
+++ C/0013-Roman-to-Integer.cpp
@@ -0,0 +1,30 @@
+    int romanToInt(string s) {
+        vector<int> nums(s.length() + 1);
+        
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == 'I') {
+                nums[i] = 1;
+            } else if (s[i] == 'V') {
+                nums[i] = 5;
+            } else if (s[i] == 'X') {
+                nums[i] = 10;
+            } else if (s[i] == 'L') {
+                nums[i] = 50;
+            } else if (s[i] == 'C') {
+                nums[i] = 100;
+            } else if (s[i] == 'D') {
+                nums[i] = 500;
+            } else {
+                nums[i] = 1000;
+            }
+        }
+        
+        nums.back() = 0;
+        int sum = 0;
+        
+        for (int i = nums.size()-1; i >= 1; --i) {
+            nums[i-1] >= nums[i] ? sum += nums[i-1] : sum -= nums[i-1];
+        }
+        
+        return sum;
+    }
