eb3f687b450e08d33479de40bc81f22d0dd404eb
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-25 17:09:14 +0800

    Update 0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp

--- C/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
+++ C/0235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
@@ -1,5 +1,5 @@
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if ((p->val >= root->val && q-> val <= root->val) || (p->val <= root->val && q-> val >= root->val)) {
+        if ((p->val >= root->val && q->val <= root->val) || (p->val <= root->val && q->val >= root->val)) {
             return root;
         } else if (root->val > p->val && root->val > q->val) {
             return lowestCommonAncestor(root->left, p, q);
