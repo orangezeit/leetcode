558353eb8297ee07de504714e592d74d9286381b
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-07-29 13:09:48 +0800

    Update 0476-Number-Complement.cpp

--- C/0476-Number-Complement.cpp
+++ C/0476-Number-Complement.cpp
@@ -1,27 +1,15 @@
     int findComplement(int num) {
-        if (num == 0) {
-            return 1;
-        }
-        
         vector<int> binary(32, 0);
-        
-        int k(0), sum(0);
+        int k(0), two(1), sum(0);
         
         while (num) {
             binary[k++] = num % 2;
             num /= 2;
         }
         
-        vector<int> twos(k,1);
-        
-        for (int i = 1; i < k; ++i) {
-            twos[i] = 2 * twos[i-1];
-        }
-        
         for (int i = 0; i < k; ++i) {
-            if (binary[i] - 1) {
-                sum += twos[i];
-            }
+            if (binary[i] - 1) sum += two;
+            two *= 2;
         }
         
         return sum;
