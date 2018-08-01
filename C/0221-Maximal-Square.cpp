86cffbfe8ecb2f6252162febc854fbd4f4ad50af
Yunfei Luo <40207006+orangezeit@users.noreply.github.com>
2018-08-01 11:57:11 +0800

    Update 0221-Maximal-Square.cpp

--- C/0221-Maximal-Square.cpp
+++ C/0221-Maximal-Square.cpp
@@ -34,8 +34,46 @@ public:
         
         return k * k;
     }
-
 // Method 2
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int k = 0;
+        
+        vector<vector<int>> dp(m ,vector<int>(n, 0));
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[i][j] += matrix[i][j] - '0';
+                if (i) dp[i][j] += dp[i-1][j];
+                if (j) dp[i][j] += dp[i][j-1];
+                if (i && j) dp[i][j] -= dp[i-1][j-1];
+            }
+        }
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int r = min(m-i,n-j); r >= k+1; --r) {
+                    int ones = dp[i+r-1][j+r-1];
+                    
+                    if (i && j) ones += dp[i-1][j-1];
+                    if (i) ones -= dp[i-1][j+r-1];
+                    if (j) ones -= dp[i+r-1][j-1];
+                    
+                    if (ones == r * r) {
+                        k = max(k, r);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return k * k;
+    }
+
+// Method 3
     int maximalSquare(vector<vector<char>>& matrix) {
         if (matrix.empty()) return 0;
         
