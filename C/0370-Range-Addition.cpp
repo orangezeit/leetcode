70de2f5cda7c7a934cf8f435b5349f9ff92e8808
Yunfei Luo <yfluo@bu.edu>
2018-12-01 12:52:34 -0500

    Create 0370-Range-Addition.cpp

---
+++ C/0370-Range-Addition.cpp
@@ -0,0 +1,14 @@
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        vector<int> ans(length, 0);
+        
+        for (int i = 0; i < updates.size(); ++i) {
+            ans[updates[i][0]] += updates[i][2];
+            if (updates[i][1] < length - 1)
+                ans[updates[i][1] + 1] -= updates[i][2];
+        }
+        
+        for (int i = 1; i < length; ++i)
+            ans[i] += ans[i-1];
+        
+        return ans;
+    }
