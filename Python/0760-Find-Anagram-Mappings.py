333b114c1db607ec1186fcd8ff857f41d7467e6e
Yunfei Luo <yfluo@bu.edu>
2018-11-30 22:05:11 -0500

    Create 0760-Find-Anagram-Mappings.py

---
+++ Python/0760-Find-Anagram-Mappings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def anagramMappings(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        
+        return [B.index(a) for a in A]
