f8a1a14023f35be22bf18edf0edd3a9e9c6a2f14
Yunfei Luo <yfluo@bu.edu>
2020-06-12 13:01:49 -0700

    Create 0023-Merge-k-Sorted-Lists.cpp

---
+++ CPP/0023-Merge-k-Sorted-Lists.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct compare {
+    bool operator()(ListNode* a, ListNode* b) const {
+        return a->val > b->val;
+    }
+};
+class Solution {
+public:
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        for (ListNode* list: lists)
+            if(list) pq.push(list);
+        ListNode* ans = new ListNode(0);
+        ListNode* temp = ans;
+        
+        while (!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+            
+            temp->next = node;
+            temp = temp->next;
+            if (node->next) pq.push(node->next);
+        }
+        temp = ans->next;
+        delete ans;
+        return temp;
+    }
+};
