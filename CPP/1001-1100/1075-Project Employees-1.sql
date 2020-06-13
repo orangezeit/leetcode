c255bd6f6c8cc3cf365b7c35c5d383ef27f848e5
Yunfei Luo <yfluo@bu.edu>
2020-06-13 19:46:52 -0400

    Create 1075-Project Employees-1.sql

---
+++ CPP/1001-1100/1075-Project Employees-1.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT Project.project_id, ROUND(AVG(Employee.experience_years), 2) AS average_years
+FROM Project JOIN Employee ON Project.employee_id = Employee.employee_id
+GROUP BY Project.project_id;
